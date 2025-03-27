db54bed97fcb0ae0b6b761b6605db12ea79551ff
sravankumar999 <sk2000anu@gmail.com>
2025-03-27 09:53:50 -0500

    Added Maven Dockerfile

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 ## INCLUDED
 - MySQL
 - Kafka
+- Maven
---
+++ maven/dock/src/test/java/com/github/sravankumar999/dock/DockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.sravankumar999.dock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
